5f4c04998bbf28a3bf38dc88434035ea8398c61a
Stefano De Pascalis <stefanodp91@gmail.com>
2017-11-09 17:06:59 +0100

    fixed css issue

--- __head.php
+++ __head.php
@@ -104,8 +104,9 @@ s0.parentNode.insertBefore(s1,s0);
 <link rel="stylesheet" type="text/css" href="http://apps.openrecordz.com/assets/js/libs/bootstrap-switch/css/bootstrap3/bootstrap-switch.min.css">
 
 <!-- font awesome -->
-<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
-<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+<!-- <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet"> -->
+<!-- <link href="assets/css/font-awesome/font-awesome.css" rel="stylesheet"> -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
     
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -177,7 +178,7 @@ if ((localStorage.getItem('lan')) =="IT") {
 </script>
 
 <!-- @TODO spostare da qua -->
-<style type="text/css">
+<!-- <style type="text/css">
 	
 
 @media (max-width: 1088px) {
@@ -216,7 +217,7 @@ if ((localStorage.getItem('lan')) =="IT") {
         overflow: hidden!important;
     }
 }
-</style>
+</style> -->
 
 <!-- chat21 -->
 <!-- <app-root></app-root>
