c6d576523b1353e5d67654f0935c6d2004826518
andrea leo <andrea.leo83@gmail.com>
2017-11-30 17:32:06 +0100

    map fix

--- scripts/views/map/DataOnMapView.js
+++ scripts/views/map/DataOnMapView.js
@@ -21,6 +21,7 @@ define([
 		zoom: 14,
 		map: null,
 		markers: [],
+		locations: [],
 		
 /*		events: {
 		   'render': 'afterRender'
@@ -36,6 +37,7 @@ define([
 			this.asLocationData=this.options.asLocationData;
 			console.log("this.asLocationData:"+this.asLocationData);
 			this.datasetMeta=this.options.datasetMeta;
+			loadLocationData(this.data.records);
 			//_g_datasetmeta=this.datasetMeta;
 //			this.position = this.options.position;
 //			this.zoom = this.options.zoom;
@@ -46,8 +48,8 @@ define([
 			console.log('DataOnMapView.render');
 			this.$el.html(this.template({asLocationData:this.asLocationData}));
 			console.log("rendered");
-			if (this.asLocationData) {
-				this.initGoogleMap(this.data.records);
+			if (this.locations>0) {
+				this.initGoogleMap();
 				//mymap2=this;
 			}
 			//setTimeout(this.resize(),100000);
@@ -81,10 +83,31 @@ define([
 //			this.map.setCenter(posLatLng);
 //		},
 		
-		
-		initGoogleMap: function(data){
-			console.log('DataOnMapView.initGoogleMap');
+		loadLocationData: function(data){
 			console.log('data',data);
+			data.forEach(function(singleData){
+				
+				//			     console.log("singleData",singleData);
+				
+								//check if all the records contains latitude e longitude fields. skip if not.
+								if (singleData._latitude && singleData._longitude) {
+									var lat = singleData._latitude;
+									var lon = singleData._longitude;
+									var id = singleData.id;
+									var title=id;
+									if (singleData._title){
+										title=singleData._title;
+									}
+					//			    location = [description, lat, lon, 1];
+									location = [id, lat, lon, title];
+									this.locations.push(location);
+								}
+							});
+		},
+	
+		initGoogleMap: function(){
+			console.log('DataOnMapView.initGoogleMap');
+			
 			var view = this;
 			
 			var posLatLng = new google.maps.LatLng(this.position.lat, this.position.lon);
@@ -99,38 +122,13 @@ define([
 
 			//create empty LatLngBounds object
 			var bounds = new google.maps.LatLngBounds();
-			var infowindow = new google.maps.InfoWindow();    
-
-			var locations = [];
-			var location;
-			data.forEach(function(singleData){
-
-//			     console.log("singleData",singleData);
-
-				//check if all the records contains latitude e longitude fields. skip if not.
-				if (singleData._latitude && singleData._longitude) {
-					var lat = singleData._latitude;
-					var lon = singleData._longitude;
-					var id = singleData.id;
-					var title=id;
-					if (singleData._title){
-						title=singleData._title;
-					}
-	//			    location = [description, lat, lon, 1];
-					location = [id, lat, lon, title];
-					locations.push(location);
-				}
-			});
+			var infowindow = new google.maps.InfoWindow();  		
 			
-			console.log("locations",locations);
-//			var locations = [
-//			                 ['DESCRIPTION3', 41.926979,12.517385, 3],
-//			                 ['DESCRIPTION2', 41.914873,12.506486, 2],
-//			                 ['DESCRIPTION1', 41.918574,12.507201, 1]
-//			               ];
-			for (i = 0; i < locations.length; i++) {  
+			console.log("this.locations",this.locations);
+
+			for (i = 0; i < this.locations.length; i++) {  
 				var marker = new google.maps.Marker({
-					position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+					position: new google.maps.LatLng(this.locations[i][1], this.locations[i][2]),
 					map: this.map
 				});
 				console.log("marker",marker);
@@ -142,7 +140,7 @@ define([
 
 				google.maps.event.addListener(marker, 'click', (function(marker, i) {
 					return function() {
-						var infowindowContent='<a href="#ds/'+view.datasetMeta._slug+'/id/'+locations[i][0]+'"><h3>'+locations[i][3]+'</h3></a>';
+						var infowindowContent='<a href="#ds/'+view.datasetMeta._slug+'/id/'+this.locations[i][0]+'"><h3>'+this.locations[i][3]+'</h3></a>';
 						infowindow.setContent(infowindowContent);
 						infowindow.open(this.map, marker);
 					}
