057c21e64e64ef472aa9b5363b64fe9e68b4b03a
Stefano De Pascalis <stefanodp91@gmail.com>
2017-12-18 19:37:35 +0100

    removed log

--- scripts/models/Tenant.js
+++ scripts/models/Tenant.js
@@ -22,8 +22,6 @@ define([
 	        	    url: tenant + '/service/v1/tenants/me',
 				type : 'GET'
 			});
-			console.log('AAAAAAAA');
-			console.log(url);
 			tenants.done(function(response){
 				console.log('Tenant.getAll.done');
 //				callback('success', response);
