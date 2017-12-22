6285f11a76550e9424659bb6db7a14f016c280ea
Stefano De Pascalis <stefanodp91@gmail.com>
2017-12-22 17:38:37 +0100

    improved tab info layout

--- assets/labels_en.js
+++ assets/labels_en.js
@@ -142,7 +142,7 @@ _label_en = {
 "dataset_detail_template_linkedin_share_label": "LinkedIn",
 
 // info_dataset_details_template
-"info_dataset_details_template_records_number": "Number of records present", 
+"info_dataset_details_template_records_number": "Number of records", 
 "info_dataset_details_template_binaries_number": "Number of binaries data", 
 "info_dataset_details_template_tags" : "Tags", 
 "info_dataset_details_template_mapping" : "Mapping",
